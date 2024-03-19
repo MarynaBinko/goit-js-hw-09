bde9732a819eacb8ce5636d064ca9cc015783f71
MarynaBinko <148637858+MarynaBinko@users.noreply.github.com>
2024-03-19 12:15:00 +0000

    Deploying to gh-pages from @ MarynaBinko/goit-js-hw-09@128853bc08e022f59e87c4cfba2c24cd152768a4 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,2 @@
-import"./assets/styles-a663f59e.js";console.log(gallery);
+import"./assets/styles-a663f59e.js";import{S as o}from"./assets/vendor-870f0eb5.js";new o(".gallery a",{});console.log(o);
 //# sourceMappingURL=commonHelpers.js.map
